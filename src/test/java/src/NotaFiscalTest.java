81c66a7d16839758afb0194a97a663d38b8a1807
tm <tm@mail.com>
2025-06-05 21:55:54 -0300

    adicionado estrutura de exemplo para testes

---
+++ src/test/java/src/NotaFiscalTest.java
@@ -0,0 +1,14 @@
+package src;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotaFiscalTest {
+
+    @Test
+    public void testExample() {
+        NotaFiscal notaFiscal = new NotaFiscal();
+        assertNotNull(notaFiscal);
+    }
+}
